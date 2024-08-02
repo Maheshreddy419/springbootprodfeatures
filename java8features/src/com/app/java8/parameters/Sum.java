775d004cac1da5350a06658536e03a6215b688f5
Maheshreddy419 <mahesh.apple62@gmail.com>
2024-08-02 13:51:02 +0530

    dwfs

--- java8features/src/com/app/java8/parameters/Sum.java
+++
@@ -1,5 +0,0 @@
-package com.app.java8.parameters;
-@FunctionalInterface
-public interface Sum {
-    int sum(int a, int b);
-}
--- java8features/src/com/app/java8/parameters/demo/StreamPrograms.java
+++
@@ -1,6 +0,0 @@
-package com.app.java8.parameters.demo;
-
-public class StreamPrograms {
-    public static void main(String[] args) {
-   }
-}
